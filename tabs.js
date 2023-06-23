d83b1d92eb21a292c7c3234aa8bbfb998b8b11e7
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2023-06-23 02:09:08 -0400

    Candlelight

--- tabs.js
+++ tabs.js
@@ -13,12 +13,92 @@ GrimoireTab.prototype.display = function() {
     // drawAlligatorQuietVert(currentProgram);
     currentProgram = getProgram("new-flickering-dots");
     gl.useProgram(currentProgram);
-    drawSwirl(currentProgram);
+    // drawSwirl(currentProgram);
     // drawDots(currentProgram);
     currentProgram = getProgram("rounded-square");
     time = gl.getUniformLocation(currentProgram, "time"); 
     disturb = gl.getUniformLocation(currentProgram, "disturb"); 
     gl.useProgram(currentProgram);
+    // makeFlame();
+    
+    drawTerminal(currentProgram);
+    // currentProgram = getProgram("new-flickering-dots");
+    // gl.useProgram(currentProgram);
+    // drawSwirlTop(currentProgram);
+    // drawSwirl(currentProgram);
+    // drawPulsar(currentProgram);
+    // unbind the buffer and draw the resulting texture....
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.viewport(0, 0, cnvs.width, cnvs.height);
+    // 
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    // 
+    gl.clearColor(0, 0, 0, 1); // clear to white
+    // 
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    // 
+    var textureShader = getProgram("textu");
+    gl.useProgram(textureShader);
+    // 
+    aspect = cnvs.width / cnvs.height;
+    let vertices = new Float32Array([-1, 1, 1, 1, 1, -1, // Triangle 1
+        -1, 1, 1, -1, -1, -1 // Triangle 2
+    ]);
+    vbuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    itemSize = 2;
+    numItems = vertices.length / itemSize;
+    textureShader.aVertexPosition = gl.getAttribLocation(textureShader, "a_position");
+    gl.enableVertexAttribArray(textureShader.aVertexPosition);
+    gl.vertexAttribPointer(textureShader.aVertexPosition, itemSize, gl.FLOAT, false, 0, 0);
+    // 
+    var textureLocation = gl.getUniformLocation(textureShader, "u_texture");
+    gl.uniform1i(textureLocation, 0);
+    var timeLocation = gl.getUniformLocation(textureShader, "time");
+    gl.uniform1f(timeLocation, drawCount * 0.01);
+    // 
+    var scalar = gl.getUniformLocation(textureShader, "resolution");
+    gl.uniform1f(scalar, resolutionScalar);
+    // 
+    var texcoordLocation = gl.getAttribLocation(textureShader, "a_texcoord");
+    gl.enableVertexAttribArray(texcoordLocation);
+    // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2; // 2 components per iteration
+    var type = gl.FLOAT; // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0; // start at the beginning of the buffer
+    gl.vertexAttribPointer(texcoordLocation, size, type, normalize, stride, offset);
+    gl.drawArrays(gl.TRIANGLES, 0, numItems);
+};
+
+    justine = ge.makeBlurredArray("quatre.scd", 0, 376, 109, 376 + 25);
+
+ge.getTab("quatre.scd").display = function() {
+    flameSize = openSimplex.noise2D(drawCount * 1e-1, drawCount * 1e-1 + 1000) * 0.5 + 0.5;
+    
+    bindFrameBuffer(texture, framebuf);
+    gl.viewport(0, 0, cnvs.width, cnvs.height);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    // draw the scene, presumably on a framebuffer
+    let currentProgram = getProgram("pulsar-fog");
+    gl.useProgram(currentProgram);
+    drawBG(currentProgram);
+    currentProgram = getProgram("new-flickering-dots-vert");
+    gl.useProgram(currentProgram);
+    // drawAlligatorQuietVert(currentProgram);
+    currentProgram = getProgram("new-flickering-dots");
+    gl.useProgram(currentProgram);
+    // drawSwirl(currentProgram);
+    // drawDots(currentProgram);
+    currentProgram = getProgram("rounded-square");
+    time = gl.getUniformLocation(currentProgram, "time"); 
+    disturb = gl.getUniformLocation(currentProgram, "disturb"); 
+    gl.useProgram(currentProgram);
+    ge.xFadeWithZeroes("quatre.scd", 0, 376, 109, 376 + 25, "quatre.scd", 0, 376 + 25, 1 - flameSize, justine);
+    // ge.xFadeWithZeroes("quatre.scd", 0, 376, 109, 376 + 25, "cinq.scd", 0, 0, flameSize, justine);
+    makeFlame();
     drawTerminal(currentProgram);
     // currentProgram = getProgram("new-flickering-dots");
     // gl.useProgram(currentProgram);
